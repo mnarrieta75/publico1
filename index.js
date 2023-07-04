aa5a89a9316e0a2ee6679708a649e95e86479410
mnarrieta75 <mnarrieta@gmail.com>
2023-07-04 15:57:18 +0200

    Commit inicial

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+const path = require("path");
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname + "/views/index.html"))
+    console.log("GET /views/index.html")
+})
+
+
+
+const PORT = 3001
+app.listen(PORT, (err) => {
+    if (err) {console.log("Error al iniciar el servidor")}
+    else { console.log("Servidor corriendo en el puerto ", PORT)}
+})
+
+
